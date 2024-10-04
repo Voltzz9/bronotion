6a1c754354304d157c1bf4c939926f5aee0afd52
Jonathan Donald <25957848@sun.ac.za>
2024-10-04 13:46:03 +0200

    Changes to service worker to display more indepth notification

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,30 +1,13 @@
-/* self.addEventListener("push", (event) => {
+self.addEventListener("push", (event) => {
     const data = event.data.json();
     const title = data.title;
     const body = data.body;
     const icon = data.icon;
-    const url = data.data.url;
 
     const notificationOptions = {
         body: body,
-        tag: "unique-tag", // Use a unique tag to prevent duplicate notifications
         icon: icon,
-        data: {
-            url: url, // Replace with the desired URL for redirecting user to the desired page
-        },
     };
 
     self.registration.showNotification(title, notificationOptions);
-});
- */
-
-console.log("Service Worker Loaded...");
-
-self.addEventListener("push", (e) => {
-    const data = e.data.json();
-    console.log("Push Recieved...");
-
-    self.registration.showNotification(data.title, {
-        body: "Knock Knock",
-    });
 });
